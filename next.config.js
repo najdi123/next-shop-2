888b2099f8752dcb0adc4a4c5ce68a472fb91adf
monire <aglida1370@gmail.com>
2023-11-15 23:28:30 +0330

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    GOOGLE_ID:
+      "498779275456-cgj4kaao0lf2j34rmo3vejo8clns4bma.apps.googleusercontent.com",
+    GOOGLE_SECRET: "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",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
